53de07679fc6e3946536fd8a362bd2d597177cef
Anuradha Singh <88154930+21Anuradha@users.noreply.github.com>
2025-05-03 09:58:44 +0000

    variabe and datatype

---
+++ basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId =345
+let accountEmail ="anu@gmail.com"
+var accountPassword = "123456"
+accountCity = "Gurgaon"
+let accoutState
+// accountId = 1255
+accountEmail = "abc@gmail.com"
+accountPassword = "123123"
+accountCity = "Delhi"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity,accoutState])
