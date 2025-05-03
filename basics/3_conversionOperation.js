5130e0d1866e0342afd352900e4d6209c32a9811
Anuradha Singh <88154930+21Anuradha@users.noreply.github.com>
2025-05-03 10:24:12 +0000

    converstion of datat type

---
+++ basics/3_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = undefined
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLoggedIn = null
+let booleasnIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleasnIsLoggedIn);
+
+let someNumber = 33
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber);
+console.log(typeof(stringSomeNumber));
+
+
