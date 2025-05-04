ffe89873f9d3a0f5f7d81ae94286d575954373d8
Anuradha Singh <88154930+21Anuradha@users.noreply.github.com>
2025-05-04 06:15:13 +0000

    revision

---
+++ basics/revision_dataType.js
@@ -0,0 +1,17 @@
+let abc
+console.log(typeof(abc))
+let b = null
+console.log(typeof(b))
+let a = true
+console.log(typeof(a))
+let c = 2
+console.log(typeof(c))
+let str = "hii"
+console.log(typeof(str))
+let cb =""
+console.log(typeof(cb))
+
+
+
+
+
