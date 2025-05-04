a23dedb2576e5f16816544119b6d350ff2e4b9c5
Anuradha Singh <88154930+21Anuradha@users.noreply.github.com>
2025-05-04 06:10:18 +0000

    datatype

--- basics/02_dataTypes.js
+++ basics/02_dataTypes.js
@@ -4,3 +4,30 @@ console.log(3 + 2)
 let age = 19;
 console.log("ANu")
 console.log(typeof undefined );
+
+//symbol
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId);
+
+
+const bigNumber= 234567890987654323456n
+console.log(bigNumber);
+
+//non primitive
+
+const heros = ["balvir","Ravi","Doga"]
+let obj = {
+    name: "anuradha",
+    age: 25,
+}
+
+//function declaration 
+const myFunction = function(){
+    console.log("Hello");
+    
+}
+
+console.log(typeof myFunction);
+
