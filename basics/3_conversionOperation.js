4b96b019dcf9f95f525a99be7eabd3cd1505f4be
Anuradha Singh <88154930+21Anuradha@users.noreply.github.com>
2025-05-04 05:43:21 +0000

    cnversion and comparsiotion

--- basics/3_conversionOperation.js
+++ basics/3_conversionOperation.js
@@ -13,7 +13,45 @@ let booleasnIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringSomeNumber = String(someNumber)
-console.log(stringSomeNumber);
-console.log(typeof(stringSomeNumber));
+// console.log(stringSomeNumber);
+// console.log(typeof(stringSomeNumber));
+
+//*************************Operations**************************
+let value =3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Bharat"
+let str2 = " Mata"
+let str3 = str1 + str2
+
+console.log(str3);
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 +2);
+console.log(2 + 2 +"1");
+
+console.log(true);
+console.log(+true);
+console.log(+ "");
+
+
+let num1, num2, num3
+num1 = num2=num3=2+2
+
+let gameCounter = 100
+console.log(gameCounter);
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
 
 
